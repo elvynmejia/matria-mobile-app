2a0e8d1d00586924815eacf32b7c6f2e956327e7
Elvyn Mejia <elvynmejia@gmail.com>
2024-01-16 10:47:35 -0800

    Sets selected user

--- App.tsx
+++ App.tsx
@@ -1,20 +1,170 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+import {
+  GluestackUIProvider,
+  Text,
+  View,
+  Input,
+  InputField,
+  FlatList,
+  Heading,
+  Box,
+  HStack,
+  Avatar,
+  AvatarImage,
+  VStack,
+  ScrollView,
+  Pressable,
+} from "@gluestack-ui/themed";
+
+import { config } from "@gluestack-ui/config"; // Optional if you want to use default theme
 
 export default function App() {
+  const [users, setUsers] = useState([]);
+  const [loading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  const [userSelected, setUserSelected] = useState(null);
+
+  /*
+  example user 
+
+  {
+    "id": 1,
+    "name": "Leanne Graham",
+    "username": "Bret",
+    "email": "Sincere@april.biz",
+    "address": {
+      "street": "Kulas Light",
+      "suite": "Apt. 556",
+      "city": "Gwenborough",
+      "zipcode": "92998-3874",
+      "geo": {
+        "lat": "-37.3159",
+        "lng": "81.1496"
+      }
+    },
+    "phone": "1-770-736-8031 x56442",
+    "website": "hildegard.org",
+    "company": {
+      "name": "Romaguera-Crona",
+      "catchPhrase": "Multi-layered client-server neural-net",
+      "bs": "harness real-time e-markets"
+    }
+  }
+  */
+  useEffect(() => {
+    const fetchUsers = async () => {
+      try {
+        const response = await axios.get(
+          "https://jsonplaceholder.typicode.com/users",
+        );
+        console.log("response.data.length", response.data.length);
+        setUsers(response.data);
+      } catch (error: any) {
+        setError(error.message);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
+    fetchUsers();
+  }, []);
+
+  const onSearch = (t: string) => {
+    // if (!t) {
+    //   return;
+    // }
+
+    const searchTerm = t.toLowerCase();
+    setUsers((prev) => {
+      return prev.filter((u: any) => {
+        const name = u.name.toLowerCase();
+        const username = u.username.toLowerCase();
+
+        return name.includes(searchTerm) || username.includes(searchTerm);
+      });
+    });
+  };
+
+  const onUserSelected = (user: any) => {
+    console.log("onUserSelected.user", user);
+    setUserSelected(user);
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <GluestackUIProvider config={config}>
+      <Box mx={10} my={50} gap={15}>
+        <Input
+          variant="rounded"
+          size="lg"
+          isDisabled={false}
+          isInvalid={false}
+          isReadOnly={false}
+        >
+          <InputField
+            placeholder="name, or @username"
+            onChangeText={onSearch}
+          />
+        </Input>
+        <Heading size="md">Contactos</Heading>
+        <FlatList
+          data={users}
+          renderItem={({ item }: { item: any }) => (
+            <Box
+              borderBottomWidth="$1"
+              borderColor="$trueGray800"
+              $dark-borderColor="$trueGray100"
+              $base-pl={0}
+              $base-pr={0}
+              $sm-pl="$4"
+              $sm-pr="$5"
+              py="$2"
+            >
+              <Pressable
+                onPress={() => onUserSelected(item)}
+              >
+                <HStack space="md" justifyContent="flex-start">
+                  {item.avatar ? (
+                    <Avatar size="md">
+                      <AvatarImage source={{ uri: "" }} alt="user avatar" />
+                    </Avatar>
+                  ) : (
+                    <Avatar size="md">
+                      <Text color="$coolGray900">{item.name[0]}</Text>
+                    </Avatar>
+                  )}
+                  <VStack>
+                    <Text
+                      color="$coolGray800"
+                      fontWeight="$normal"
+                      $dark-color="$warmGray100"
+                    >
+                      {item.name}
+                    </Text>
+                    <Text color="$coolGray600" $dark-color="$warmGray200">
+                      {item.username}
+                    </Text>
+                  </VStack>
+                </HStack>
+              </Pressable>
+            </Box>
+          )}
+          keyExtractor={(item: any) => item.id}
+        />
+      </Box>
+    </GluestackUIProvider>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+// });
---
+++ README.md
@@ -0,0 +1,3 @@
+```bash
+ nvm use v20.9.0 # set node version
+```
